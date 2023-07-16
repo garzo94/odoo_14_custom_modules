7b9df6111a16f4e2954bb2b8eddd5f16174deac2
root <root@Alex>
2023-07-15 22:21:09 -0600

    custom security permissions

---
+++ absorben_custom_security/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2014 Serv. Tec. Avanzados - Pedro M. Baeza
+# Copyright 2014 Oihane Crucelaegui - AvanzOSC
+# Copyright 2018 Simone Rubino - Agile Business Group
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Permisos Personalizados - Absorben",
+    "version": "14.0.1.0.1",
+    "category": "Tools",
+    "license": "AGPL-3",
+    "author": "Alexander Garzo",
+    "website": "https://github.com/garzo94",
+    "depends": [],
+    "data": [
+        "security/security.xml",
+    ],
+
+    "installable": True,
+    "auto_install": False,
+}
